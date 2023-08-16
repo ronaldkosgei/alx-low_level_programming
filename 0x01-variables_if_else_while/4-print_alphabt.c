1734888749ae6fa497edb62be590e773b09910fd
ronaldkosgei <kosgeironald4@gmail.com>
2023-08-16 12:50:41 -0700

    prints the alphabet in lowercase, followed by a new lin

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * excluding the letters 'q' and 'e',
+ * using the putchar function twice.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+}
+
+putchar('\n');
+
+return (0);
+}
