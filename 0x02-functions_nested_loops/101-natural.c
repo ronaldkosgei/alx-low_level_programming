7ec3217a8785f1cf72e83a2e9ed6e21ca930accc
ronaldkosgei <kosgeironald4@gmail.com>
2023-08-17 03:52:02 -0700

    list all the natural numbers below 10 that are multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point 
- * Descrption: Lists all the natural numbers below 1024 (excluded)
+ * Descrption:Lists all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
