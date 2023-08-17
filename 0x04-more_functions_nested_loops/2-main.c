5cb59660deccf4950fd95eedf879fb8d94fd228d
ronaldkosgei <kosgeironald4@gmail.com>
2023-08-17 15:26:03 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int a = 2;
+int b = 3;
+
+printf("%d * %d = %d\n", a, b, mul(a, b));
+
+return (0);
+}
