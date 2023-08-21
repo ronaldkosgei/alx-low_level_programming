17a8c9866c525bebaf1ef66805f1ff73a5292eb0
ronaldkosgei <kosgeironald4@gmail.com>
2023-08-21 03:15:53 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -3,13 +3,15 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always return 0.
  */
+void puts2(char *str);
+
 int main(void)
 {
-char *str;
+char str[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.Hletn";
 
-str = "0123456789";
 puts2(str);
+
 return (0);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * puts2 - Prints every other character of a string, starting with the first character
+ * puts2 - Prints every other character of a string, starting with
+ * the first character
  * @str: The input string
- *
- * Description: This function takes a string as input and prints every other character,
- *              starting with the first character, followed by a new line.
  */
 void puts2(char *str)
 {
@@ -13,8 +11,11 @@ int i = 0;
 
 while (str[i] != '\0')
 {
+if (i % 2 == 0)
+{
 putchar(str[i]);
-i += 2; /* Increment by 2 to skip every other character */
+}
+i++;
 }
 
 putchar('\n'); /* Print a new line */
