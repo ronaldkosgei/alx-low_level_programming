04ca303664e94e19e80e9cef721bc5780b759c76
ronaldkosgei <kosgeironald4@gmail.com>
2023-08-30 11:58:01 +0300

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - Prints the sum of the diagonals of a square matrix.
- * @a: A pointer to the square matrix of integers.
- * @size: The size of the square matrix (number of rows/columns).
- *
- * Description: This function calculates the sum of the primary diagonal
- * and the sum of the secondary diagonal of the matrix and prints them.
+ * sum_of_diagonals - Calculates and prints the sum of
+ * the diagonals of a square matrix.
+ * @matrix: The square matrix.
+ * @n: The size of the matrix.
  */
-void print_diagsums(int *a, int size)
+void sum_of_diagonals(int matrix[][3], int n)
 {
-int sum_primary = 0;/* Sum of the primary diagonal */
-int sum_secondary = 0;/* Sum of the secondary diagonal */
+int primary_sum = 0;
+int secondary_sum = 0;
 int i;
-for (i = 0; i < size; i++)
+for (i = 0; i < n; i++)
 {
-sum_primary += a[i * size + i];/* Access primary diagonal element */
-sum_secondary += a[i * size + (size - 1 - i)];
-/* Access secondary diagonal element */
+primary_sum += matrix[i][i];
+secondary_sum += matrix[i][n - 1 - i];
 }
-printf("Sum of primary diagonal: %d\n", sum_primary);
-printf("Sum of secondary diagonal: %d\n", sum_secondary);
+printf("Sum of primary diagonal: %d\n", primary_sum);
+printf("Sum of secondary diagonal: %d\n", secondary_sum);
 }
