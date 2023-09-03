178e0820def66ffd0c2786092460438a2cf2a39b
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-03 12:57:28 +0300

    1. Silence is argument carried out by other means mandatory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @void: No parameters expected
+ *
+ * Return: 0 on success
+ */
+int main(void) {
+printf("0\n");
+/* No command-line arguments (excluding program name) */
+return (0);
+}
