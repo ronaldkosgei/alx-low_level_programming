a15b3e71fea19b1fcac89c57b86bda315c176d4a
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-05 12:20:59 +0300

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string to concatenate to the first.
+ *
+ * Return: A newly allocated string containing the concatenated result,
+ *         or NULL if memory allocation fails.
+ */
+char *str_concat(char *s1, char *s2)
+{
+size_t len1, len2, total_len;
+char *result;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+total_len = len1 + len2 + 1; /* +1 for the null-terminator*/
+result = (char *)malloc(total_len * sizeof(char));
+if (result == NULL)
+return (NULL);
+strcpy(result, s1);
+strcat(result, s2);
+return (result);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
 
 #endif /* MAIN_H */
