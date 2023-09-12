f80c212cb04557654ec45973cf91a51278d4c1b9
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-12 11:47:53 +0300

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog (a string).
+ * @age: The age of the dog (a floating-point number).
+ * @owner: The owner of the dog (a string).
+ *
+ * Description: This structure defines a data type to store information
+ * about a dog, including its name, age, and owner. It is used to create
+ * instances of dog records.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
