373fe5ac3bab4b9be8bf9b9d540e057a8df46162
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-14 10:37:35 +0300

    1. A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,39 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "dog.h"
+#include <stddef.h>
 
-/* Define the 'struct dog' type*/
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
 /**
- * init_dog - Initializes a 'struct dog' with the provided values.
- * @d: Pointer to the 'struct dog' to be initialized.
+ * init_dog - Initializes a struct dog.
+ * @d: Pointer to the struct dog to initialize.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- *
- * Description: This function initializes a 'struct dog' with the provided
- * name, age, and owner values. If 'd' is NULL, no initialization is performed.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog new_dog;
-/* Allocate memory for the 'name' and 'owner' strings and copy the values*/
-new_dog.name = strdup(name);
-new_dog.age = age;
-new_dog.owner = strdup(owner);
-return (new_dog);
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
-/* Function to free memory allocated for 'struct dog' variable*/
-void free_dog(struct dog *d)
-{
-if (d == NULL)
-{
-return;
-}
-free(d->name);
-free(d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,7 @@ dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 /* Add any other function prototypes here */
 
 #endif /* DOG_H */
