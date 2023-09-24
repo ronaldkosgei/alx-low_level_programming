1a906359d5588b6d4776f1d10d43e268cff37e62
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-24 09:58:00 +0300

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void tortoise_and_hare_message(void) __attribute__((constructor));
+
+/**
+ * tortoise_and_hare_message - Prints the message before main.
+ */
+void tortoise_and_hare_message(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
