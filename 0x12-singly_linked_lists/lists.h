cc6d4ed6d38f8170601ad434add155a05390e889
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-24 09:35:27 +0300

    2. Add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ size_t list_len(const list_t *h);
 void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LIST_H */
