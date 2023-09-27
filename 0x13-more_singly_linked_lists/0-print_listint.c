0a033b6768cd03b48533e65d25be638f895ce7d8
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-27 13:39:05 +0300

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <stddef.h>
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * print_listint - Prints all elements of a listint_t list.
+ * @h: Pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
-size_t node_count = 0;
+size_t count = 0;
+/* Initialize a counter to keep track of the number of nodes*/
 while (h != NULL)
 {
 printf("%d\n", h->n);
-h = h->next;
-node_count++;
+/* Print the current node's value*/
+h = h->next; /* Move to the next node*/
+count++; /* Increment the count*/
 }
-return (node_count);
+return (count); /* Return the total number of nodes*/
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,31 @@
-#include <stdlib.h>
+#include <stddef.h>
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely.
- * @h: Pointer to a pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t list.
+ * @h: A pointer to a pointer to the head of the listint_t list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t count = 0;
+size_t node_count = 0;
 listint_t *current = *h;
-listint_t *next_node;
+listint_t *next;
 while (current != NULL)
 {
-count++;
-next_node = current->next;
+next = current->next;
+node_count++;
 free(current);
-current = next_node;
-/* Check for a circular reference */
-if (current >= *h)
+current = next;
+/* Check if we've reached the original head again (loop detected) */
+if (current == *h)
 {
-*h = NULL;
+*h = NULL; /* Set the head to NULL to indicate a loop */
 break;
 }
 }
-*h = NULL;
-return (count);
+return (node_count);
 }
