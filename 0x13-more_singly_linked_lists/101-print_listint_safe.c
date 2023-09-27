5af0c1b4925def81593cee0cf4fe67d183c2ad59
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-27 14:09:50 +0300

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,18 +10,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *current = head;
+const listint_t *slow_ptr = head;
+const listint_t *fast_ptr = head;
 size_t count = 0;
-while (current != NULL)
+while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
 {
-printf("[%p] %d\n", (void *)current, current->n);
+printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 count++;
-if (current <= current->next)
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next->next;
+if (slow_ptr == fast_ptr)
 {
-printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-exit(98); /* Exit with status 98 if there's a loop in the list*/
+printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+exit(98);/* Exit with status 98 if a loop is detected*/
 }
-current = current->next;
 }
 return (count);
 }
