067428af131e66620a745ccc07cf044c9023d7ce
ronaldkosgei <kosgeironald4@gmail.com>
2023-09-28 10:52:34 +0300

    Correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,8 @@
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string containing 0s and 1s.
  *
- * Return: The converted number, or 0 if there is an invalid character or b is NULL.
+ * Return: The converted number, or 0 if there is an invalid
+ * character or b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,11 +5,12 @@
  * @n: The unsigned long integer.
  * @index: The index of the bit to retrieve (starting from 0).
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurs.
+ * Return: The value of the bit at the specified index,
+ * or -1 if an error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
-return (int)((n >> index) & 1);
+return ((int)((n >> index) & 1));
 }
